a636bff62f6d9635c160c9194bd8d32511397486
Doudmur <bbeka544@mail.ru>
2022-07-13 16:46:18 +0600

    second commit

--- src/figure.java
+++ src/figure.java
@@ -9,3 +9,19 @@ class triangle extends figure{
 class quadrilateral extends figure{
 
 }
+
+class pentagon extends figure{
+
+}
+
+class hexagon extends figure{
+
+}
+
+class oval extends figure{
+
+}
+
+class circle extends figure{
+
+}
